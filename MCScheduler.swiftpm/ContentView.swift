d1bf2877bb59bb84e13ddd243bcac1bf80fe7dc7
Nick Nalli <nalli.nikhil@gmail.com>
2023-09-23 07:49:38 -0500

    Add the new project to the repo and also added the necessary git ignore files

---
+++ MCScheduler.swiftpm/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            CircleImage()
+            
+            VStack {
+                Text("Hello, class!")
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+                HStack {
+                    Text("Intro to Playground").font(.body)
+                    Spacer()
+                    Text("Xcode").font(.subheadline)
+                }
+            }
+            .padding()
+        }
+    }
+}
