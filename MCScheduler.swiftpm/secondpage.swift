c6d3cded4b51e41779bb510a7cf2c22bb2282f0d
pratigya <sudeepbiswa0@gmail.com>
2023-10-07 13:03:50 -0500

    pratigya

--- MCScheduler.swiftpm/secondpage.swift
+++ MCScheduler.swiftpm/secondpage.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 struct secondpage: View {
     var body: some View{
-        ZStack{
-            Form{
-                TextField("Enter your reminder", text: value) {
-                    DatePicker(selection:.constant(Date((),)                              <#T##() -> _#>)
+        
+        @State var date = Date()
+        @State var reminder: String = ""
+        @State var location: String = ""
+        
+        ZStack {
+            Form {
+            
+                TextField("Enter your reminder", text: $reminder)
+                TextField("Location", text: $location)
+                DatePicker("Date", selection: $date)
+            }
+            VStack(spacing: 20) {
+               
+                
+                HStack {
+                    
+                    Button("Save") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 70, height:40)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .padding()
+                    
+                    Button("Cancel") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 70, height:40)
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .cornerRadius(10)
                 }
             }
         }
     }
-                .cornerRadius(10)
-        }
+}
+    
+
+struct secondpage_Previews: PreviewProvider {
+    static var previews: some View {
+        secondpage()
     }
+}
