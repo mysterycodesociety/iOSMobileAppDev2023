94f8df4e180a5eaeabbcb6e8a36156f19ef3c492
DevTatsuya <pikachutrained@icloud.com>
2023-10-14 12:56:58 -0500

    I added an alert

--- MCScheduler.swiftpm/Main Page.swift
+++ MCScheduler.swiftpm/Main Page.swift
@@ -10,23 +10,32 @@ import SwiftUI
 
 struct MainPage: View {
     @State private var text = ""
+    @State private var date = Date()
+    
+    @State private var Saved = false
     var body: some View{
         NavigationView {
             ZStack {
                 Form {
                     TextField("Enter your reminder", text: $text)
-                    DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+                    DatePicker(selection: $date, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
                 }
                 HStack {
-                    NavigationLink(destination: MainPage().navigationBarBackButtonHidden(true)){
-                        Text("Save").frame(width:100, height:75, alignment: .center).background(Color.blue).foregroundColor(Color.white).cornerRadius(99)
-                    }.navigationBarBackButtonHidden(true)
+                    Button("Save") {
+                        Saved = true
+                    }.frame(width:100, height:75).background(Color.blue).foregroundColor(Color.white).cornerRadius(99)
+                    
+                    
                     
                     NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)){
                         Text("Cancel").frame(width:100, height:75, alignment: .center).background(Color.red).foregroundColor(Color.white).cornerRadius(99)
                     }.navigationBarBackButtonHidden(true)
                 }
             }
+        }.alert("Saved", isPresented: $Saved) {
+            
+        }message: {
+            Text("Your Reminder: \(text) for \(date) has been set")
         }
     }
 }
