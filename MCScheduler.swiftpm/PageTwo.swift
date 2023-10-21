4fd483a8c92597c744837da7c2852ec0a209d466
Esha Vadde <eshareddy.vadde@gmail.com>
2023-10-21 13:08:42 -0500

    object and class

--- MCScheduler.swiftpm/PageTwo.swift
+++ MCScheduler.swiftpm/PageTwo.swift
@@ -10,35 +10,40 @@ import SwiftUI
 struct PageTwo: View {
     var body: some View {
         NavigationView {
-            
-            @State var date = Date()
-            @State var location: String = ""
-            @State var reminder: String = ""
-            ZStack() {
-                Form {
-                    TextField("Enter Your Reminder", text: $reminder)
-                    TextField("Enter Location", text: $location)
-                    DatePicker("Date", selection: $date)
-                }
-                
-                HStack {
-                   Button("Save") {
-                   }
-                   .frame(width: 81, height: 30)
-                   .font(.title)
-                   .foregroundColor(Color.white)
-                   .background(Color.purple)
-                   .cornerRadius(10)
-                    .padding()
+           
+//            @State var date = Date()
+//            @State var location: String = ""
+//            @State var reminder: String = ""
+                ZStack {
+                    Form {
+                        TextField("Enter Your Reminder", text: reminderObj.init().reminder)
+                        TextField("Enter Location", text: "".init().location)
+                        DatePicker("Date", selection: Date())
+                    }
                     
-                    NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)){
-                        Text("Cancel").font(.title).frame(width:95, height:30, alignment: .center).background(Color.red).foregroundColor(Color.white).cornerRadius(10)
-                    }.navigationBarBackButtonHidden(true)
+                    HStack {
+                        Button("Save") {
+                        }
+                        .frame(width: 81, height: 30)
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .background(Color.purple)
+                        .cornerRadius(10)
+                        .padding()
+                        
+                        NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)){
+                            Text("Cancel").font(.title).frame(width:95, height:30, alignment: .center).background(Color.red).foregroundColor(Color.white).cornerRadius(10)
+                        }.navigationBarBackButtonHidden(true)
+                    }
                 }
-                
-                
-            }
         }
+    }
+    
+    struct reminderObj{
+        var date = Date()
+        var location: String = ""
+        var reminder: String = ""
+        
     }
     
     struct PageTwo_Previews: PreviewProvider {
