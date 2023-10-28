873d10e83fef33255a28a7f5cd515f935bcd7ec0
DevTatsuya <pikachutrained@icloud.com>
2023-10-28 10:43:02 -0500

    Finished

--- MCScheduler.swiftpm/View/AddView.swift
+++ MCScheduler.swiftpm/View/AddView.swift
@@ -5,20 +5,3 @@
 //  Created by Student on 10/21/23.
 //
 
-import SwiftUI
-
-struct AddView: View {
-    var body: some View {
-        ScrollView {
-            Text ("Hi")
-        }
-        .navigationTitle ("Add an Item !*)
-    }
-}
-            
-struct AddView_Previews: PreviewProvider {
-        static var previews: some VIew
-            NavigationView
-                AddView()
-    }
-}
